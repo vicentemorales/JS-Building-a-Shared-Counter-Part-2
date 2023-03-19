901960d7f36d37de7ad02463c974e132a3120ce4
vicentemorales <vmorales121@gmail.com>
2023-03-19 13:04:39 -0700

    done

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const resetButton = document.querySelector('#Reset');
 
     const response = await fetch('http://localhost:9001/counter');
 
@@ -12,15 +13,36 @@ async function main(){
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        update()
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        update()
+    }
+
+    function resetTest(){
+        countValue = 11;
+        countContainer.textContent = countValue;
+        
+    }
+
+    async function update(){
+        await fetch('http://localhost:9001/counter',{
+            method:'PATCH',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({ 
+            'value': countValue
+            })
+        })
     }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    resetButton.addEventListener('click', resetTest)
     countContainer.textContent = countValue;
 }
 main()
